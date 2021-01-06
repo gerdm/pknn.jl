40606d43008e54ef6d8ee5b1074948887ba7855d
Gerardo Duran <g.duran@me.com>
2021-01-06 07:46:05 +0000

    style: clean moons.jl example

--- examples/moons.jl
+++ examples/moons.jl
@@ -5,6 +5,8 @@ using StatsPlots
 
 X, y = Pknn.Utils.make_moons(n_samples=150, noise=0.3, random_state=314)
 target_samples = 3_000
+
+# Initial sampling configuration
 config = [
     Dict("beta"=>10, "k"=>1, "eta"=>1.1),
     Dict("beta"=>5, "k"=>5, "eta"=>1.1), 
@@ -20,27 +22,29 @@ N_hist = 1:size(k_samples)[2]
 
 active_samples = hcat(reshape(k_samples, (:,1)), reshape(beta_samples, (:, 1)))
 
-l = @layout [a b; c [d; e]]
-colors = [yn == 1 ? "darkorange" : "turquoise3" for yn in y]
-Nx, Ny = 50, 50
-p1 = Pknn.Utils.plot_surface(X, y, active_samples, Nx, Ny)
-scatter!(X[:, 1], X[:, 2], color=colors,
-             label=nothing, markerstrokewidth=0, marker=:+)
-plot!(title="Dataset")
+begin
+    l = @layout [a b; c [d; e]]
+    colors = [yn == 1 ? "darkorange" : "turquoise3" for yn in y]
+    Nx, Ny = 50, 50
+    p1 = Pknn.Utils.plot_surface(X, y, active_samples, Nx, Ny)
+    scatter!(X[:, 1], X[:, 2], color=colors,
+                label=nothing, markerstrokewidth=0, marker=:+)
+    plot!(title="Dataset")
 
-p2 = Pknn.Utils.freqplot(k_samples[1, :], label="k s(1)", alpha=0.5)
-Pknn.Utils.freqplot!(k_samples[2, :], label="k s(2)", alpha=0.5)
-plot!(title="k-samples")
+    p2 = Pknn.Utils.freqplot(k_samples[1, :], label="k s(1)", alpha=0.5)
+    Pknn.Utils.freqplot!(k_samples[2, :], label="k s(2)", alpha=0.5)
+    plot!(title="k-samples")
 
-p3 = histogram(beta_samples[1, :], label="beta s(1)", alpha=0.5)
-histogram!(beta_samples[2, :], label="beta s(2)", alpha=0.5)
-plot!(title="β-samples")
+    p3 = histogram(beta_samples[1, :], label="beta s(1)", alpha=0.5)
+    histogram!(beta_samples[2, :], label="beta s(2)", alpha=0.5)
+    plot!(title="β-samples")
 
-p4 = plot(beta_samples[1, :], label=nothing)
-plot!(beta_samples[2, :], label=nothing, title="β-hist")
+    p4 = plot(beta_samples[1, :], label=nothing)
+    plot!(beta_samples[2, :], label=nothing, title="β-hist")
 
-p5 = plot(k_samples[1, :], label=nothing)
-plot!(k_samples[2, :], label=nothing)
-plot!(title="k-hist")
+    p5 = plot(k_samples[1, :], label=nothing)
+    plot!(k_samples[2, :], label=nothing)
+    plot!(title="k-hist")
 
-plot(p1, p2, p3, p4, p5, layout=l)
+    plot(p1, p2, p3, p4, p5, layout=l)
+end
